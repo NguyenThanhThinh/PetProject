9476d7c0c72168cfaaa6bcc474e92f603c73d719
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-02-25 21:57:59 +0700

    add ex1

---
+++ PetProject/Angular/Scripts/spa/app.js
@@ -0,0 +1,14 @@
+﻿var myAppp = angular.module('myApp', []);
+myAppp.controller('studentController', studentController);
+myAppp.controller('teacherController', teacherController);
+myAppp.controller('schoolController', schoolController);
+
+function schoolController($scope) {
+    $scope.message = "Hello school";
+}
+function studentController($scope) {
+    //$scope.message = "Hello student";
+}
+function teacherController($scope) {
+    //$scope.message = "Hello teacher";
+}
